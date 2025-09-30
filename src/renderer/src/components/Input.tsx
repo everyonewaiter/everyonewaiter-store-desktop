55ce4dd7b4acda7028857eb05182a5481a123a9f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-09-30 15:00:19 +0900

    style: Input css 수정

--- src/renderer/src/components/Input.tsx
+++ src/renderer/src/components/Input.tsx
@@ -13,7 +13,7 @@ function Input({ className, hasError, ...props }: CombineInputProps) {
       data-error={hasError}
       aria-invalid={hasError}
       className={cn(
-        "text-gray-0 md:text-s font-regular w-full border py-2.5 pl-3 outline-none placeholder:text-gray-300 disabled:border-gray-500 disabled:bg-gray-700 disabled:text-gray-300 disabled:placeholder:text-gray-400 md:h-9 md:rounded-lg md:pr-3 lg:h-12 lg:rounded-xl lg:pr-3 lg:text-[15px]",
+        "text-gray-0 md:text-s font-regular w-full border py-2.5 pr-3 outline-none placeholder:text-gray-300 disabled:border-gray-500 disabled:bg-gray-700 disabled:text-gray-300 disabled:placeholder:text-gray-400 md:h-9 md:rounded-lg md:pl-3 lg:h-12 lg:rounded-xl lg:pl-4 lg:text-[15px]",
         hasError ? "border-status-error" : "border-gray-600",
         className
       )}
