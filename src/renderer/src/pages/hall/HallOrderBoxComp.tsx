366cba229e680ad1cd1e45bab105ad330fcde3f3
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-05 17:04:29 +0900

    feat: HallOrderBoxComp 레이아웃 개선 및 옵션 표시 처리

--- src/renderer/src/pages/hall/HallOrderBoxComp.tsx
+++ src/renderer/src/pages/hall/HallOrderBoxComp.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@renderer/components";
 import HallOrderOptionComp from "@renderer/pages/hall/HallOrderOptionComp";
-import { OrderMenu } from "@renderer/types/domain";
+import { OrderMenu, OrderOption } from "@renderer/types/domain";
 import cn from "@renderer/utils/cn";
 
 interface HallOrderBoxCompProps {
@@ -9,19 +9,27 @@ interface HallOrderBoxCompProps {
 }
 
 function HallOrderBoxComp({ orderMenu, isCompleted = false }: HallOrderBoxCompProps) {
-  // TODO: false인 경우 레이아웃 깨짐 현상 해결 필요
+  // TODO: API에서 showImage 설정값 받아와서 적용 필요 (현재는 임시로 true)
   const showImage = true;
   const isServed = orderMenu.served;
+
+  const orderOptions: (OrderOption & { orderOptionGroupId: string })[] =
+    orderMenu.orderOptionGroups.flatMap((orderOptionGroup) =>
+      orderOptionGroup.orderOptions.map((orderOption) => ({
+        ...orderOption,
+        orderOptionGroupId: orderOptionGroup.orderOptionGroupId,
+      }))
+    );
 
   return (
     <div
       className={cn(
-        "flex flex-col justify-center gap-6 rounded-xl border border-gray-600 p-6",
+        "flex min-h-[279px] flex-col justify-between gap-6 rounded-xl border border-gray-600 p-6",
         isServed ? "bg-[#F1F1F1]" : ""
       )}
     >
       <div className="flex flex-col gap-6">
-        {showImage && (
+        {showImage && !isServed && (
           <div className="relative h-[130px] w-[130px] overflow-hidden rounded-xl">
             <div className="absolute inset-0 bg-[#F1F1F1] opacity-50" />
             {/* TODO: 테스트용 이미지 교체 필요 */}
@@ -46,12 +54,23 @@ function HallOrderBoxComp({ orderMenu, isCompleted = false }: HallOrderBoxCompPr
           </div>
           {orderMenu.orderOptionGroups.length > 0 && (
             <div className="flex flex-col gap-1">
-              {orderMenu.orderOptionGroups
-                .flatMap((orderOptionGroup) => orderOptionGroup.orderOptions)
-                .map((orderOption, index) => (
-                  // TODO: key로 index 사용 지양, orderOptionId가 없으므로 대안 고민 필요
-                  <HallOrderOptionComp key={index} orderOption={orderOption} isServed={isServed} />
-                ))}
+              {orderOptions.slice(0, 2).map((orderOption) => (
+                <HallOrderOptionComp
+                  key={`${orderOption.orderOptionGroupId}-${orderOption.name}`}
+                  orderOption={orderOption}
+                  isServed={isServed}
+                />
+              ))}
+              {orderOptions.length > 2 && (
+                <div
+                  className={cn(
+                    "flex items-center justify-between",
+                    isServed ? "text-gray-300" : "text-[#2E7BB3]"
+                  )}
+                >
+                  <div className="pl-2">...</div>
+                </div>
+              )}
             </div>
           )}
         </div>
