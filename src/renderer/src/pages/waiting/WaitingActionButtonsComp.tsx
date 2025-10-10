9767b593797aff59aed13334ee68dec59640f137
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-10 17:55:51 +0900

    fix: 웨이팅 액션 버튼 반응형으로 통합

--- src/renderer/src/pages/waiting/WaitingActionButtonsComp.tsx
+++ src/renderer/src/pages/waiting/WaitingActionButtonsComp.tsx
@@ -7,42 +7,24 @@ interface WaitingActionButtonsCompProps {
 
 function WaitingActionButtonsComp({ callCount }: WaitingActionButtonsCompProps) {
   return (
-    <>
-      <nav className="items-center gap-5 md:hidden lg:flex">
-        {WAITING_ACTIONS.map((action, index) => (
-          <Button
-            key={action.label}
-            className="flex h-30 w-30 cursor-pointer flex-col gap-0.5 rounded-[20px]"
-            style={{ color: action.textColor }}
-            {...action}
-          >
+    <nav className="flex md:w-30 md:flex-col md:gap-3 lg:w-auto lg:flex-row lg:items-center lg:gap-5">
+      {WAITING_ACTIONS.map((action, index) => (
+        <Button
+          key={action.label}
+          className="flex cursor-pointer flex-col gap-0.5 md:min-h-14 md:rounded-2xl md:!px-6 md:!py-4 lg:h-30 lg:min-h-0 lg:w-30 lg:rounded-[20px] lg:!px-0 lg:!py-0"
+          style={{ color: action.textColor }}
+          {...action}
+        >
+          <div className="flex md:flex-row md:items-center md:gap-1 lg:flex-col lg:gap-0.5">
             {action.icon}
-            <span className="text-xl font-semibold">{action.label}</span>
-            {callCount > 0 && index === 0 && (
-              <span className="text-sm font-medium">총 {callCount}회 · 15분전</span>
-            )}
-          </Button>
-        ))}
-      </nav>
-      <nav className="w-30 flex-col gap-3 md:flex lg:hidden">
-        {WAITING_ACTIONS.map((action, index) => (
-          <Button
-            key={action.label}
-            className="flex min-h-14 flex-col gap-0.5 rounded-2xl !px-6 !py-4"
-            style={{ color: action.textColor }}
-            {...action}
-          >
-            <div className="flex items-center gap-1">
-              {action.icon}
-              <span className="font-regular text-lg">{action.label}</span>
-            </div>
-            {callCount > 0 && index === 0 && (
-              <span className="text-sm font-medium">총 {callCount}회 · 15분전</span>
-            )}
-          </Button>
-        ))}
-      </nav>
-    </>
+            <span className="font-semibold md:text-lg lg:text-xl">{action.label}</span>
+          </div>
+          {callCount > 0 && index === 0 && (
+            <span className="text-sm font-medium">총 {callCount}회 · 15분전</span>
+          )}
+        </Button>
+      ))}
+    </nav>
   );
 }
 
