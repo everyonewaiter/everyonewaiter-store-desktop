c312cedc927959cde4050314c6a4a0c5243d4860
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-10 17:59:25 +0900

    refactor: WAITING_ACTIONS 상수를 컴포넌트 내부로 이동

--- src/renderer/src/pages/waiting/WaitingActionButtonsComp.tsx
+++ src/renderer/src/pages/waiting/WaitingActionButtonsComp.tsx
@@ -1,5 +1,39 @@
+import { ReactElement } from "react";
+import { BellRingingIcon, DoorOpenIcon, XCircleIcon } from "@renderer/assets/icons";
 import { Button } from "@renderer/components";
-import { WAITING_ACTIONS } from "@renderer/pages/waiting/constants";
+import { ButtonColor, ButtonVariant } from "@renderer/components/Button/Button.types";
+
+interface WaitingActionType {
+  icon: ReactElement<SVGElement>;
+  label: string;
+  variant: ButtonVariant["variant"];
+  color: ButtonColor;
+  textColor: string;
+}
+
+const WAITING_ACTIONS: WaitingActionType[] = [
+  {
+    icon: <BellRingingIcon className="md:h-6 md:w-6 lg:h-10 lg:w-10" />,
+    label: "호출",
+    variant: "default",
+    color: "primary",
+    textColor: "white",
+  },
+  {
+    icon: <DoorOpenIcon className="md:h-6 md:w-6 lg:h-10 lg:w-10" />,
+    label: "입장",
+    variant: "outline",
+    color: "primary",
+    textColor: "primary",
+  },
+  {
+    icon: <XCircleIcon className="md:h-6 md:w-6 lg:h-10 lg:w-10" />,
+    label: "취소",
+    variant: "default",
+    color: "grey",
+    textColor: "#999999",
+  },
+];
 
 interface WaitingActionButtonsCompProps {
   callCount: number;
