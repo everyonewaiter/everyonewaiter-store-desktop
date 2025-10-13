c9042f700b28828a6af9c751412fd6e2de7f73c8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-13 23:09:01 +0900

    chore: 정적 파일 추가

--- src/renderer/src/assets/icons/index.ts
+++ src/renderer/src/assets/icons/index.ts
@@ -1,8 +1,15 @@
+export { default as AlarmIcon } from "@renderer/assets/icons/alarm.svg?react";
 export { default as BabyIcon } from "@renderer/assets/icons/baby.svg?react";
 export { default as BellRingingIcon } from "@renderer/assets/icons/bell-ringing.svg?react";
 export { default as ChevronDownIcon } from "@renderer/assets/icons/chevron-down.svg?react";
+export { default as ChevronLeftIcon } from "@renderer/assets/icons/chevron-left.svg?react";
+export { default as ChevronRightIcon } from "@renderer/assets/icons/chevron-right.svg?react";
 export { default as DoorOpenIcon } from "@renderer/assets/icons/door-open.svg?react";
+export { default as DoubleArrowIcon } from "@renderer/assets/icons/double-arrow.svg?react";
+export { default as FileCheckIcon } from "@renderer/assets/icons/file-check.svg?react";
 export { default as InfoIcon } from "@renderer/assets/icons/info.svg?react";
 export { default as PlusIcon } from "@renderer/assets/icons/plus.svg?react";
+export { default as ReturnIcon } from "@renderer/assets/icons/return.svg?react";
 export { default as SmileIcon } from "@renderer/assets/icons/smily.svg?react";
+export { default as StopwatchIcon } from "@renderer/assets/icons/stopwatch.svg?react";
 export { default as XCircleIcon } from "@renderer/assets/icons/x-circle.svg?react";
