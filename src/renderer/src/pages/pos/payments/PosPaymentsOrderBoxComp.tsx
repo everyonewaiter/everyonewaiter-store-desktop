e44fed121a046f935cf575ddc80fabd97ed34c42
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-14 00:01:22 +0900

    feat: 포스 결제내역 페이지 퍼블리싱

---
+++ src/renderer/src/pages/pos/payments/PosPaymentsOrderBoxComp.tsx
@@ -0,0 +1,57 @@
+import { Fragment } from "react/jsx-runtime";
+import { PlusIcon } from "@renderer/assets/icons";
+import { Checkbox } from "@renderer/components";
+import { PosOrderMockType } from "@renderer/pages/pos/mock";
+
+export default function PosPaymentsOrderBoxComp({ children }: { children: React.ReactNode }) {
+  return <div className="flex flex-col gap-2">{children}</div>;
+}
+
+PosPaymentsOrderBoxComp.Order = function Order({ order }: { order: PosOrderMockType }) {
+  return (
+    <div className="flex flex-col gap-2 rounded-xl border border-gray-600 p-4">
+      <div className="flex items-center justify-between">
+        <span className="text-lg font-medium text-gray-100">{order.name}</span>
+        <span className="text-lg font-medium text-gray-100">{order.quantity}개</span>
+      </div>
+      <div className="flex flex-col gap-1">
+        {order.orderOptionGroups.map((item) =>
+          item.orderOptions.map((option) => (
+            <div key={item.orderOptionGroupId} className="flex items-center justify-between">
+              <Fragment>
+                <span className="flex text-base font-medium text-[#2E7BB3]">
+                  <PlusIcon width={24} height={24} color="#2E7BB3" />
+                  {item.name}
+                </span>
+                <span className="flex text-base font-medium text-[#2E7BB3]">{option.name}</span>
+              </Fragment>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+PosPaymentsOrderBoxComp.Index = function Number({
+  index,
+  hasCheckbox = false,
+}: {
+  index: number;
+  hasCheckbox?: boolean;
+}) {
+  return (
+    <div className="relative flex items-center gap-3">
+      {hasCheckbox && <Checkbox />}
+      <strong className="text-gray-0 text-2xl font-semibold">{index + 1}</strong>
+    </div>
+  );
+};
+
+PosPaymentsOrderBoxComp.Divider = function Divider() {
+  return (
+    <div className="w-full py-6">
+      <div className="h-[1px] w-full bg-gray-600" />
+    </div>
+  );
+};
---
+++ src/renderer/src/pages/pos/payments/PosPaymentsPage.tsx
@@ -0,0 +1,67 @@
+import { Button, DatePicker, Table } from "@renderer/components";
+import { PAYMENTS_MOCK, POS_ORDER_MOCK } from "@renderer/pages/pos/mock";
+import PosPaymentsSideComp from "@renderer/pages/pos/payments/PosPaymentsSideComp";
+import PosHeaderComp from "@renderer/pages/pos/PosHeaderComp";
+
+const COLUMN_WIDTHS = {
+  number: "flex-[1]",
+  cash: "flex-[1.8]",
+  card: "flex-[1.8]",
+  total: "flex-[1.8]",
+  status: "flex-[1.4]",
+  createdAt: "flex-[1.8]",
+};
+
+export default function PosPaymentsPage() {
+  return (
+    <div className="flex min-h-dvh flex-col overflow-y-hidden">
+      <PosHeaderComp />
+      <div className="relative flex w-full flex-1">
+        <div className="flex h-[calc(100dvh-133px)] flex-[calc(1-0.3375)] flex-col gap-6 overflow-y-auto px-15 py-8">
+          <DatePicker date={new Date()} onSetDate={() => {}} />
+          <Table className="">
+            <Table.Header className="flex w-full">
+              <Table.Row className="flex w-full cursor-default">
+                <Table.Head className={COLUMN_WIDTHS.number}>번호</Table.Head>
+                <Table.Head className={COLUMN_WIDTHS.cash}>현금</Table.Head>
+                <Table.Head className={COLUMN_WIDTHS.card}>카드</Table.Head>
+                <Table.Head className={COLUMN_WIDTHS.total}>합계</Table.Head>
+                <Table.Head className={COLUMN_WIDTHS.status}>상태</Table.Head>
+                <Table.Head className={COLUMN_WIDTHS.createdAt}>시간</Table.Head>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {PAYMENTS_MOCK.map((item) => (
+                <Table.Row className="flex w-full" key={item.posTableActivityId}>
+                  <Table.Cell className={COLUMN_WIDTHS.number}>{item.number}</Table.Cell>
+                  <Table.Cell className={COLUMN_WIDTHS.cash}>
+                    {`${item.cash.toLocaleString()} 원`}
+                  </Table.Cell>
+                  <Table.Cell className={COLUMN_WIDTHS.card}>
+                    {`${item.card.toLocaleString()} 원`}
+                  </Table.Cell>
+                  <Table.Cell className={COLUMN_WIDTHS.total}>
+                    {`${item.total.toLocaleString()} 원`}
+                  </Table.Cell>
+                  <Table.Cell className={COLUMN_WIDTHS.status}>
+                    <Button
+                      color={item.status === "APPROVE" ? "approve" : "reject"}
+                      className="button-sm"
+                    >
+                      {item.status === "APPROVE" ? "승인" : "취소"}
+                    </Button>
+                  </Table.Cell>
+                  <Table.Cell className={COLUMN_WIDTHS.createdAt}>
+                    {item.createdAt.split(":")[0]}시 {item.createdAt.split(":")[1]}분{" "}
+                    {item.createdAt.split(":")[2]}초
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+        <PosPaymentsSideComp data={POS_ORDER_MOCK} />
+      </div>
+    </div>
+  );
+}
