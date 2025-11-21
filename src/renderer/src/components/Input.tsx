23135c4062ee0926c29a33c47ca3042513f652e1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-11-22 01:13:15 +0900

    fix: form 사용으로 인해 Input 컴포넌트 수정

--- src/renderer/src/components/Input.tsx
+++ src/renderer/src/components/Input.tsx
@@ -1,31 +1,45 @@
-import { ReactElement } from "react";
+import { forwardRef, ReactElement } from "react";
+import { ControllerFieldState } from "react-hook-form";
 import cn from "@renderer/utils/cn";
 
 interface InputProps {
-  hasError?: boolean;
   prefix?: ReactElement;
+  fieldState?: ControllerFieldState;
 }
 
 type CombineInputProps = Omit<React.ComponentProps<"input">, "prefix"> & InputProps;
 
-function Input({ className, hasError, prefix, ...props }: CombineInputProps) {
-  return (
-    <div className="relative h-12 flex-1">
-      {prefix && <div className="absolute top-1/2 left-4 h-5 w-5 -translate-y-1/2">{prefix}</div>}
-      <input
-        data-slot="form-input"
-        data-error={hasError}
-        aria-invalid={hasError}
-        className={cn(
-          "text-gray-0 h-12 w-full rounded-xl border py-2.5 pr-3 pl-4 text-[15px] font-normal outline-none placeholder:text-gray-300 disabled:border-gray-500 disabled:bg-gray-700 disabled:text-gray-300 disabled:placeholder:text-gray-400",
-          hasError ? "border-status-error" : "border-gray-600",
-          prefix && "pl-10",
-          className
-        )}
-        {...props}
-      />
-    </div>
-  );
-}
+const Input = forwardRef<HTMLInputElement, CombineInputProps>(
+  ({ className, prefix, fieldState, ...props }, ref) => {
+    const hasError = !!fieldState?.error;
+    const errorMessage = fieldState?.error?.message;
+
+    return (
+      <div className="flex w-full flex-1 flex-col gap-1">
+        <div className="relative h-12">
+          {prefix && (
+            <div className="absolute top-1/2 left-4 h-5 w-5 -translate-y-1/2">{prefix}</div>
+          )}
+          <input
+            ref={ref}
+            data-slot="form-input"
+            data-error={hasError}
+            aria-invalid={hasError}
+            className={cn(
+              "text-gray-0 h-12 w-full rounded-xl border py-2.5 pr-3 pl-4 text-[15px] font-normal outline-none placeholder:text-gray-300 disabled:border-gray-500 disabled:bg-gray-700 disabled:text-gray-300 disabled:placeholder:text-gray-400",
+              hasError ? "border-status-error" : "border-gray-600",
+              prefix && "pl-10",
+              className
+            )}
+            {...props}
+          />
+        </div>
+        {errorMessage && <p className="text-status-error ml-1 text-xs">{errorMessage}</p>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
