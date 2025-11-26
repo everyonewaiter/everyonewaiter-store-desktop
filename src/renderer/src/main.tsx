c47057519d9fe66ecd8cd4039549aecc024bf1e8
handwoong <handwoong@gmail.com>
2025-11-26 15:18:54 +0900

    chore: react query 환경 설정

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@renderer/App";
+import { queryClient } from "@renderer/queries/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { OverlayProvider } from "overlay-kit";
 import "@renderer/assets/main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <OverlayProvider>
-      <App />
-    </OverlayProvider>
+    <QueryClientProvider client={queryClient}>
+      <OverlayProvider>
+        <App />
+      </OverlayProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
