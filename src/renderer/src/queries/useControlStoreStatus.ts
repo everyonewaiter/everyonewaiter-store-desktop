eb93989e0044cad05aca0641c166c3c255d2c5b9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-01 20:40:58 +0900

    fix: sse 사용으로 인해 invalidateQueries문 삭제

--- src/renderer/src/queries/useControlStoreStatus.ts
+++ src/renderer/src/queries/useControlStoreStatus.ts
@@ -1,6 +1,4 @@
 import { api } from "@renderer/api";
-import { queryClient } from "@renderer/queries/client";
-import { queryKey } from "@renderer/queries/key";
 import { useMutation } from "@tanstack/react-query";
 
 export const useControlStoreStatus = () => {
@@ -8,17 +6,11 @@ export const useControlStoreStatus = () => {
     mutationFn: async () => {
       await api.post("/stores/open");
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey.STORE] });
-    },
   });
 
   const close = useMutation({
     mutationFn: async () => {
       await api.post("/stores/close");
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey.STORE] });
     },
   });
 
