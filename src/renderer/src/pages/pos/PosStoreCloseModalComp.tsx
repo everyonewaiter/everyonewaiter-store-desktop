3c0de9ee09c5a31a453ccdc91f58562fd1dd5ec4
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-05 23:10:12 +0900

    feat: POS 페이지 매장 오픈/마감 스위치 구현 및 모달 통합

--- src/renderer/src/pages/pos/PosStoreCloseModalComp.tsx
+++ src/renderer/src/pages/pos/PosStoreCloseModalComp.tsx
@@ -1,29 +1,37 @@
 import { Dialog } from "@renderer/components/Dialog";
 import { useControlStoreStatus } from "@renderer/queries/useControlStoreStatus";
+import { useGetDevice } from "@renderer/queries/useGetDevice";
+import { useGetStore } from "@renderer/queries/useGetStore";
 import { ModalProps } from "@renderer/types/overlay";
 
 interface PosStoreCloseModalCompProps extends ModalProps {
-  onSuccess: () => void;
+  onSuccess?: () => void;
 }
 
 function PosStoreCloseModalComp({ onSuccess, ...props }: PosStoreCloseModalCompProps) {
-  const { closeStore } = useControlStoreStatus();
+  const { device } = useGetDevice();
+  const { store } = useGetStore(device?.storeId ?? "");
+  const { openStore, closeStore } = useControlStoreStatus();
 
   return (
     <Dialog open={props.isOpen} onOpenChange={props.close}>
       <Dialog.Wrapper>
         <div className="text-gray-0 flex items-center justify-center py-6 text-xl font-semibold">
-          매장을 마감하시겠습니까?
+          매장을 {store?.status === "CLOSE" ? "오픈" : "마감"}하시겠습니까?
         </div>
         <Dialog.Footer
           layout="balanced"
           primaryButton={{
             color: "primary",
-            text: "마감하기",
+            text: store?.status === "CLOSE" ? "오픈하기" : "마감하기",
             onClick: async () => {
-              await closeStore.mutateAsync();
+              if (store?.status === "CLOSE") {
+                await openStore.mutateAsync();
+              } else {
+                await closeStore.mutateAsync();
+              }
               props.close();
-              onSuccess();
+              onSuccess?.();
             },
           }}
         />
