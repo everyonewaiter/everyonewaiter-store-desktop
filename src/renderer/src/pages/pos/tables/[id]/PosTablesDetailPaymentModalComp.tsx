5f5fc1162e2862e0a2f726522e4cfe43c9120308
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-18 00:48:46 +0900

    fix: 결제 모달 퍼블리싱 수정

--- src/renderer/src/pages/pos/tables/[id]/PosTablesDetailPaymentModalComp.tsx
+++ src/renderer/src/pages/pos/tables/[id]/PosTablesDetailPaymentModalComp.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Dropdown, Input } from "@renderer/components";
 import { Dialog } from "@renderer/components/Dialog";
-import { TableActivity } from "@renderer/types/domain";
+import { PaymentSchema, paymentSchema } from "@renderer/schemas/pos";
+import { OrderReceiptType, TableActivity } from "@renderer/types/domain";
 import { ModalProps } from "@renderer/types/overlay";
-import { getFormattedTableNo } from "@renderer/utils/format";
+import cn from "@renderer/utils/cn";
+import {
+  formatBusinessNumber,
+  formatPhoneNumber,
+  formatPrice,
+  getFormattedMenuName,
+  getFormattedTableNo,
+} from "@renderer/utils/format";
 
 const cardInstallmentMonths = new Array(12)
   .fill(0)
@@ -10,18 +21,18 @@ const cardInstallmentMonths = new Array(12)
     i === 0 ? { name: "일시불", id: "0" } : { name: `${i + 1}개월`, id: String(i + 1) }
   );
 
-const cashReceiptTypes = [
+const cashReceiptTypes: { label: string; value: OrderReceiptType }[] = [
   {
     label: "신청안함",
-    value: "none",
+    value: "NONE",
   },
   {
     label: "개인소득공제용",
-    value: "individual",
+    value: "DEDUCTION",
   },
   {
     label: "사업자증빙용",
-    value: "business",
+    value: "PROOF",
   },
 ];
 
@@ -35,14 +46,32 @@ function PosTablesDetailPaymentModalComp({
   activity,
   ...props
 }: PosTablesDetailPaymentModalCompProps) {
+  const form = useForm<PaymentSchema>({
+    resolver: zodResolver(paymentSchema),
+    defaultValues: {
+      paymentAmount: String(activity.remainingPaymentPrice),
+      cashReceiptType: "NONE",
+      cashReceiptNumber: "",
+      installment: "0",
+    },
+  });
+
   const handlePayment = async () => {
-    // TODO: 결제 로직에 필요한 데이터 가져올 수 있도록 변경
-    // TODO: 이 함수 구현은 아래 데이터를 console.log로 찍어주세요.
-    // 결제할 금액 (사용자 입력)
-    // 선택한 현금 영수증 타입
-    // 입력한 현금 영수증 휴대폰 번호 또는 사업자 번호
-    // 카드 할부 개월
+    console.log({
+      paymentAmount: form.watch("paymentAmount"), // 결제할 금액
+      cashReceiptType: form.watch("cashReceiptType"), // 현금 영수증 타입
+      cashReceiptNumber: form.watch("cashReceiptNumber").replaceAll("-", ""), // 휴대폰/사업자번호
+      installment: form.watch("installment").padStart(2, "0"), // 카드 할부 개월
+    });
   };
+
+  const cashReceiptInputLabel =
+    form.watch("cashReceiptType") === "DEDUCTION" ? "휴대폰 번호" : "사업자번호";
+
+  useEffect(() => {
+    form.setValue("cashReceiptNumber", "", { shouldValidate: false });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [form.watch("cashReceiptType")]);
 
   return (
     <Dialog open={props.isOpen} onOpenChange={props.close}>
@@ -55,29 +84,30 @@ function PosTablesDetailPaymentModalComp({
             <div className="flex flex-col gap-2">
               <span className="text-gray-0 text-[15px] font-normal">결제 정보</span>
               <h3 className="text-gray-0 text-2xl font-semibold">
-                {
-                  // TODO: orderMenus.length가 1인경우와 2이상인 경우 분기처리
-                  // 1인 경우 알리오 올리오
-                  // 2이상인 경우 알리오 올리오 외 n개
-                }
-                {activity.orders[0].orderMenus[0].name} 외{" "}
-                {activity.orders
-                  .map((order) => order.orderMenus.length)
-                  .reduce((a, b) => a + b, 0) - 1}
-                개
+                {getFormattedMenuName(activity.orders.flatMap((order) => order.orderMenus))}
               </h3>
             </div>
-            {
-              // TODO: 결제 금액을 수정할 수 있도록 입력창으로 변경
-              // 기본값 activity.remainingPaymentPrice
-              // 최대값 activity.remainingPaymentPrice
-            }
             <div className="flex flex-col gap-2">
               <span className="text-gray-0 text-[15px] font-normal">결제할 금액</span>
-              <h3 className="text-gray-0 text-2xl font-semibold">
-                {activity.totalOrderPrice.toLocaleString()}원
-              </h3>
+              <div className="flex items-center gap-2">
+                <Input
+                  className="text-gray-0 text-lg! font-semibold"
+                  value={
+                    form.watch("paymentAmount")
+                      ? Number(form.watch("paymentAmount")).toLocaleString()
+                      : ""
+                  }
+                  onChange={(e) => {
+                    const val = formatPrice(e.target.value, activity.remainingPaymentPrice);
+                    if (val !== null) form.setValue("paymentAmount", val);
+                  }}
+                  placeholder={String(activity.remainingPaymentPrice.toLocaleString())}
+                  fieldState={form.getFieldState("paymentAmount")}
+                />
+                <h3 className="text-gray-0 text-2xl font-semibold">원</h3>
+              </div>
             </div>
+
             {paymentType === "cash" ? (
               <>
                 <div className="flex flex-col gap-2">
@@ -88,33 +118,54 @@ function PosTablesDetailPaymentModalComp({
                         key={receiptType.value}
                         variant="outline"
                         color="grey"
-                        className="button-lg focus:border-primary focus:text-primary flex-1 border border-gray-500 text-base !font-medium text-gray-200"
+                        className={cn(
+                          "button-lg flex-1 border border-gray-500 text-base !font-medium text-gray-200",
+                          form.watch("cashReceiptType") === receiptType.value
+                            ? "border-primary text-primary"
+                            : ""
+                        )}
+                        onClick={() => form.setValue("cashReceiptType", receiptType.value)}
                       >
                         {receiptType.label}
                       </Button>
                     ))}
                   </div>
                 </div>
-                <div className="flex flex-col gap-2">
-                  {
-                    // TODO: 선택된 receiptType에 따라 텍스트 변경
-                    // 신청안함 아래 span 및 input 숨기기
-                    // 개인소득공제 span 텍스트 휴대폰 번호, input placeholder 휴대폰 번호
-                    // 사업자증빙용 span 텍스트 사업자번호, input placeholder 사업자번호
-                  }
-                  <span className="text-gray-0 text-[15px] font-normal">휴대폰 번호</span>
-                  <Input placeholder="휴대폰 번호 / 사업자번호" className="w-full" />
-                </div>
+                {form.watch("cashReceiptType") !== "NONE" && (
+                  <div className="flex flex-col gap-2">
+                    <span className="text-gray-0 text-[15px] font-normal">
+                      {cashReceiptInputLabel}
+                    </span>
+                    <Input
+                      placeholder={cashReceiptInputLabel}
+                      className="w-full"
+                      {...form.register("cashReceiptNumber", {
+                        onChange: (e) => {
+                          const format =
+                            form.watch("cashReceiptType") === "DEDUCTION"
+                              ? formatPhoneNumber
+                              : formatBusinessNumber;
+                          return form.setValue("cashReceiptNumber", format(e), {
+                            shouldValidate: false,
+                          });
+                        },
+                      })}
+                      fieldState={form.getFieldState("cashReceiptNumber")}
+                    />
+                  </div>
+                )}
               </>
             ) : (
               <div className="flex flex-col gap-2">
-                {
-                  // TODO: default값은 일시불로 설정
-                }
                 <span className="text-gray-0 text-[15px] font-normal">할부 개월</span>
                 <Dropdown
+                  value={form.watch("installment")}
                   dropdownItems={cardInstallmentMonths}
-                  defaultText="할부개월을 선택해주세요."
+                  defaultText={
+                    cardInstallmentMonths.find((month) => month.id === form.watch("installment"))
+                      ?.name || "일시불"
+                  }
+                  onChange={(e) => form.setValue("installment", e.id)}
                 />
               </div>
             )}
