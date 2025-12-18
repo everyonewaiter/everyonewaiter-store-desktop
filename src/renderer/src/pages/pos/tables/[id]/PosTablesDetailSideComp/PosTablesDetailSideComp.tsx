8b19401e0847a45a4068f91e60cfa194b42614f1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-18 12:37:41 +0900

    rename: 컴포넌트 명 변경 CancelPayment -> CancelOrder

--- src/renderer/src/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailSideComp.tsx
+++ src/renderer/src/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailSideComp.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PosTablesDetailCancelPaymentModalComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailCancelPaymentModalComp";
+import PosTablesDetailCancelOrderModalComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailCancelOrderModalComp";
 import PosTablesDetailCheckoutSideComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailCheckoutSideComp";
 import PosTablesDetailOrderSideComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailOrderSideComp";
 import { useGetTableActivity } from "@renderer/pages/pos/tables/[id]/usePosTablesDetailApi";
@@ -20,7 +20,7 @@ function PosTablesDetailSideComp({ type = "checkout", tableNo }: PosTablesDetail
 
   const handleCancelPayment = () => {
     overlay.open((overlayProps) => (
-      <PosTablesDetailCancelPaymentModalComp
+      <PosTablesDetailCancelOrderModalComp
         tableNo={tableNo}
         checkedOrders={checkedOrders}
         activity={activity!}
