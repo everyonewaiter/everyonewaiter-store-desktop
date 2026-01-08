cb6f9baf5999f0ec1d86820d0116dd2c8a06bb33
handwoong <handwoong@gmail.com>
2026-01-09 00:48:05 +0900

    feat: 인스턴스 단일화

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,72 +4,93 @@ import { app, BrowserWindow, dialog, MessageBoxOptions, shell } from "electron";
 import { autoUpdater } from "electron-updater";
 import { join } from "path";
 
-function createWindow(): void {
-  const mainWindow = new BrowserWindow({
-    show: false,
-    fullscreen: true,
-    autoHideMenuBar: true,
-    resizable: false,
-    alwaysOnTop: true,
-    webPreferences: {
-      preload: join(__dirname, "../preload/index.js"),
-      sandbox: false,
-      contextIsolation: true,
-    },
-  });
+let mainWindow: BrowserWindow | null = null;
 
-  mainWindow.on("ready-to-show", () => {
-    mainWindow.show();
-  });
+const gotTheLock = app.requestSingleInstanceLock();
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
-    return { action: "deny" };
-  });
+if (gotTheLock) {
+  function createWindow(): void {
+    mainWindow = new BrowserWindow({
+      show: false,
+      fullscreen: true,
+      autoHideMenuBar: true,
+      resizable: false,
+      alwaysOnTop: true,
+      webPreferences: {
+        preload: join(__dirname, "../preload/index.js"),
+        sandbox: false,
+        contextIsolation: true,
+      },
+    });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  autoUpdater.on("update-downloaded", (_) => {
-    const option: MessageBoxOptions = {
-      type: "question",
-      buttons: ["업데이트", "나중에"],
-      defaultId: 0,
-      title: "UPDATER",
-      message: "프로그램 업데이트를 진행하시겠습니까?",
-    };
-
-    dialog.showMessageBox(mainWindow, option).then(function (res) {
-      if (res.response == 0) {
-        autoUpdater.quitAndInstall();
+    mainWindow.on("ready-to-show", () => {
+      if (mainWindow) {
+        mainWindow.show();
       }
+    });
+
+    mainWindow.webContents.setWindowOpenHandler((details) => {
+      shell.openExternal(details.url);
+      return { action: "deny" };
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    autoUpdater.on("update-downloaded", (_) => {
+      if (mainWindow) {
+        const option: MessageBoxOptions = {
+          type: "question",
+          buttons: ["업데이트", "나중에"],
+          defaultId: 0,
+          title: "UPDATER",
+          message: "프로그램 업데이트를 진행하시겠습니까?",
+        };
+
+        dialog.showMessageBox(mainWindow, option).then(function (res) {
+          if (res.response == 0) {
+            autoUpdater.quitAndInstall();
+          }
+        });
+      }
+    });
+
+    if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+      mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+    } else {
+      mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
+    }
+  }
+
+  app.whenReady().then(() => {
+    electronApp.setAppUserModelId("com.everyonewaiter");
+
+    app.on("browser-window-created", (_, window) => {
+      optimizer.watchWindowShortcuts(window);
+    });
+
+    registerIpcHandlers();
+
+    createWindow();
+    autoUpdater.checkForUpdates();
+
+    app.on("activate", function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
   });
 
-  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
-    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
-  } else {
-    mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
-  }
+  app.on("second-instance", () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+      mainWindow.focus();
+    }
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+} else {
+  app.quit();
 }
-
-app.whenReady().then(() => {
-  electronApp.setAppUserModelId("com.everyonewaiter");
-
-  app.on("browser-window-created", (_, window) => {
-    optimizer.watchWindowShortcuts(window);
-  });
-
-  registerIpcHandlers();
-
-  createWindow();
-  autoUpdater.checkForUpdates();
-
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
