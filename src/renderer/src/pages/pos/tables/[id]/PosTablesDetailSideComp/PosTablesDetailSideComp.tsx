4bea8fec3d265f76a634105b825131c299dd7964
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-02-05 16:59:16 +0900

    feat: 주문 승인을 위한 Side Content를 TablesDetailSideComp에 연결

--- src/renderer/src/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailSideComp.tsx
+++ src/renderer/src/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailSideComp.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGetTableActivity } from "@renderer/hooks/usePosTablesDetailApi";
 import PosTablesDetailCancelOrderModalComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailCancelOrderModalComp";
+import PosTablesDetailApprovalSideComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailApprovalSideComp";
 import PosTablesDetailCheckoutSideComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailCheckoutSideComp";
 import PosTablesDetailOrderSideComp from "@renderer/pages/pos/tables/[id]/PosTablesDetailSideComp/PosTablesDetailOrderSideComp";
 import { Order } from "@renderer/types/domain";
@@ -39,8 +40,11 @@ function PosTablesDetailSideComp({
       className="sticky top-0 right-0 flex h-dvh flex-[0.3375] flex-col gap-8 overflow-y-hidden rounded-tl-[40px] rounded-bl-[40px] pt-10 pr-4 pb-8 pl-8"
       style={{ boxShadow: "-2px 0px 20px 0px rgba(0, 0, 0, 0.08)" }}
     >
-      {type === "order" && <PosTablesDetailOrderSideComp tableNo={tableNo} />}
-      {type === "checkout" && (
+      {activity?.hasPendingOrder && <PosTablesDetailApprovalSideComp tableNo={tableNo} />}
+      {!activity?.hasPendingOrder && type === "order" && (
+        <PosTablesDetailOrderSideComp tableNo={tableNo} />
+      )}
+      {!activity?.hasPendingOrder && type === "checkout" && (
         <PosTablesDetailCheckoutSideComp
           tableNo={tableNo}
           checkedOrders={checkedOrders}
